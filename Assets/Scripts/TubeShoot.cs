9354ac9e030759db15551880cd21bad75420fa8b
MemorySharp <letovsy@yandex.ru>
2022-12-15 01:08:09 +0300

    Add shooting function

--- Assets/Scripts/TubeShoot.cs
+++ Assets/Scripts/TubeShoot.cs
@@ -7,6 +7,8 @@ public class TubeShoot : MonoBehaviour
     [SerializeField] Vector2 _xClamp;
     [SerializeField] Vector2 _zClamp;
     [SerializeField] Ball _ballPrefab;
+    [SerializeField] Transform _shootPoint;
+    [SerializeField] float _forcePower = 20f;
     void Update()
     {
         Ray ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
@@ -14,13 +16,13 @@ public class TubeShoot : MonoBehaviour
         if (plane.Raycast(ray, out _distance))
         {
             Vector3 point = ray.GetPoint(_distance);
-            Debug.Log(point);
             Vector3 pointClamp = new Vector3(Mathf.Clamp(point.x, _xClamp.x, _xClamp.y), 0f, Mathf.Clamp(point.z, _zClamp.x, _zClamp.y));
             transform.rotation = Quaternion.LookRotation(pointClamp - transform.position);
         }
         if (Input.GetMouseButtonDown(0))
         {
-            //Instantiate();
+            Ball newBall = Instantiate(_ballPrefab, _shootPoint.position, Quaternion.identity);
+            newBall.GetComponent<Rigidbody>().AddForce(_shootPoint.forward * _forcePower, ForceMode.Impulse);
         }
     }
 }
